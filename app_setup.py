f464e377a1b51b0db11884fbb3eb6f911869b23a
Yinnie Akenroye <155371795+Yinnie01@users.noreply.github.com>
2024-12-28 06:36:29 +0100

    Update SQL database URI to cloud-based database server.

--- app_setup.py
+++ app_setup.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SECRET_PROJECT'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/Yinnie/Desktop/SOFTWARE DEVELOPMENT/CODECADEMY/Learn Flask/clothing recommendation project/app/clothing_store.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/Yinnie/Desktop/SOFTWARE DEVELOPMENT/CODECADEMY/Learn Flask/clothing recommendation project/app/clothing_store.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fashionicondb_user:3GgTIxad58hhVT1F6hkrB4LdKdtDqFDc@dpg-ctnmc4tumphs73c7v7a0-a/fashionicondb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
